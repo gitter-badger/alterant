e2ef5865a3e7cd0be2448ffb9858a8d0c451a3ba
Andrew Rynhard <andrewrynhard@autonomy.io>
2016-04-21 16:16:13 -0700

    Fix openssl build script

--- script/build-openssl-static.sh
+++ script/build-openssl-static.sh
@@ -9,9 +9,9 @@ cd $SUBMODULE_PATH
 mkdir -p $INSTALL_PATH/lib
 
 FLAGS="--prefix=$INSTALL_PATH no-shared -fPIC"
-if [ $(uname) == "Darwin" ]; then
+if [ $(uname) = "Darwin" ]; then
     ./Configure darwin64-x86_64-cc $FLAGS
-elif [ $(uname) == "Linux" ]; then
+elif [ $(uname) = "Linux" ]; then
     ./config threads $FLAGS -DOPENSSL_PIC
 fi
 
